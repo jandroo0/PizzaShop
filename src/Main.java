738c65a166c1c0ccf7e5dc302dcdea59ccf4b55b
Alejandro <alejandrogarciasoto02@gmail.com>
2023-09-02 14:09:54 -0400

    Basic Specifications outline

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame  frame = new JFrame();
+        frame.setSize(800,600);
+        frame.setTitle("");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(true);
+        frame.setVisible(true);
+
+        frame.getContentPane().setBackground(Color.BLACK);
+
+//        ImageIcon image = new ImageIcon("");
+//        frame.setIconImage(image.getImage());
+
+
     }
 }
